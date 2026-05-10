cdc12bc649fd0fa50936fb9b63e74f1d16eb23ff
ilya-4-30 <ilya6423@mail.ru>
2026-05-10 21:06:32 +0300

    add animation scroll

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Бургер меню
 const burger = document.querySelector('.burger');
 
 burger.addEventListener('click', function() {
@@ -15,3 +16,31 @@ burger.addEventListener('click', function() {
 
   document.body.classList.toggle('no-scroll');
 })
+
+
+// Анимация при скролле
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        if (entry.target.classList.contains('info__counters')
+          || entry.target.classList.contains('result__list')
+          || entry.target.classList.contains('trainers__list')
+          || entry.target.classList.contains('pack__list')) {
+
+            const cards = entry.target.querySelectorAll('.info__counters-card,.result__item, .trainers__item, .pack__item');
+
+            cards.forEach((card, index) => {
+              setTimeout(() => {
+                card.classList.add('animation-on-scroll');
+              }, index * 550);
+            });
+          } else {
+            entry.target.classList.add('animation-on-scroll');
+          }
+
+        observer.unobserve(entry.target);
+      }
+    });
+}, { threshold: 1 });
+
+document.querySelectorAll('.fade-up, .info__counters, .result__list, .trainers__list, .pack__list').forEach(el => observer.observe(el));
